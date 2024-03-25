291f76136e7f48ed7a36499ae246050225899da9
Khoi Nguyen <s4052683@rmit.edu.vn>
2024-03-25 10:32:19 +0700

    Implemented Activity 3

--- Day 10 - JS/exercise-3/script.js
+++ Day 10 - JS/exercise-3/script.js
@@ -1,3 +1,6 @@
 console.log("Hello world!");
 
 // Add your js code below here!
+document.querySelector("#item-list").addEventListener("click", function(e) {
+    e.target.classList.toggle("completed");
+});
