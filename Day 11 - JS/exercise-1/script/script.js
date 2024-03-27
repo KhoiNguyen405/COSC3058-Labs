375263b662e2116c476fadf5b88208fa4d26e156
Khoi Nguyen <s4052683@rmit.edu.vn>
2024-03-27 08:28:38 +0700

    Implemented Activity 1.

--- Day 11 - JS/exercise-1/script/script.js
+++ Day 11 - JS/exercise-1/script/script.js
@@ -1,5 +1,17 @@
 function togglePattern() {
   // Enter your code here!
   console.log("Hello world!")
-  
+
+  let revealButton = document.querySelector("#patternButton");
+  let videoDiv = document.querySelector("#pattern");
+
+  revealButton.classList.toggle("revealed");
+
+  if (revealButton.classList.contains("revealed")) {
+    revealButton.innerText = "Hide pattern";
+    videoDiv.style.display = "block";
+  } else {
+    revealButton.innerText = "Reveal!";
+    videoDiv.style.display = "none";
+  }
 }
